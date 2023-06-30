30946070ca01dc671c8f309880611c2ddea6c986
Francois Gutierrez <GutierJF@basfad.basf.net>
2023-06-30 11:09:58 +0200

    latest update

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -42,7 +42,7 @@ export default function VideoPlayer({ videoSrc }: VideoPlayerProps)
       resizedDetections.forEach(detection => {
         const box = detection.detection.box
         context.beginPath()
-        context.rect(box.x, box.y, 50, 50) // Cambiamos el tamaño del cuadrado a 50x50
+        context.rect(box.x, box.y, box.width, box.height) // Cambiamos el tamaño del cuadrado a 50x50
         context.lineWidth = 2
         context.strokeStyle = 'red'
         context.fillStyle = 'red'
@@ -59,18 +59,19 @@ export default function VideoPlayer({ videoSrc }: VideoPlayerProps)
       requestAnimationFrame(runFaceDetection)
     }
 
-    video.onloadeddata = runFaceDetection
+    video.onloadeddata = () => {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      runFaceDetection();
+    }
   }, [])
 
   return (
-    <div>
-      <video ref={videoRef} autoPlay muted loop controls={true} width="640" height="420">
-      {/*  Modify the source so it has a defined width and height of 640x420 and the video controlers for play and pause*/}
-
-      <source src={videoSrc} type="video/mp4"  />
-      
+    <div style={{ position: 'relative', width: '640px', height: '480px' }}>
+      <video ref={videoRef} autoPlay muted loop controls={true} style={{ position: 'absolute', top: 0, left: 0, width: '640px', height: '480px' }}>
+        <source src={videoSrc} type="video/mp4"  />
       </video>
-      <canvas ref={canvasRef} style={{position: 'absolute'}} /> {/* Superponemos el canvas al video */}
-    </div>
+      <canvas ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0 }} />
+   </div>
   )
 }
