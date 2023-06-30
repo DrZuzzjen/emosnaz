e4229a61916c9aa1718819f91aff2e074e5eccad
Francois Gutierrez <GutierJF@basfad.basf.net>
2023-06-30 09:03:38 +0200

    adding face-api and loading models

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from 'react'
 import useSWR from 'swr'
 import PersonComponent from '../components/Person'
 import type { Person } from '../interfaces'
+import * as faceapi from 'face-api.js' // Asegúrate de haber instalado face-api.js
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
+const loadModels = async () => {
+  await faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+  await faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+  await faceapi.nets.faceExpressionNet.loadFromUri('/models')
+}
+
 export default function Index() {
   const { data, error, isLoading } = useSWR<Person[]>('/api/people', fetcher)
+
+  useEffect(() => {
+    loadModels()
+  }, [])
 
   if (error) return <div>Failed to load</div>
   if (isLoading) return <div>Loading...</div>
